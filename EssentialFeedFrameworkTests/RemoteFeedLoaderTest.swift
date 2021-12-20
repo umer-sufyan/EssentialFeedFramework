f3ce930faca7044f1c918be4670bd5d837b6b365
Umer <umersufyan@gmail.com>
2021-12-20 09:29:39 +0500

    Deliver invalid data error on 200 HTTP response with invalid json

--- EssentialFeedFrameworkTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedFrameworkTests/RemoteFeedLoaderTest.swift
@@ -10,14 +10,14 @@
 //Data URL  (covered)
 //1. Execute "Load Feed Items" command with above data.(covered)
 //2. System downloads data from the URL.(covered)
-//3. Sysem validate downloaded data.
+//3. Sysem validate downloaded data. (covered)
 //4. System creates feed item from valid data.
 //5. Systen delivers feed items.
 
 //Invalid data - errors course (sad path)
-//1. System delivers error.
+//1. System delivers error. (covered)
 
-//No connectivity - error course (sad path)
+//No connectivity - error course (sad path) (covered)
 
 
 import XCTest
@@ -107,6 +107,18 @@ class RemoteFeedLoaderTest: XCTestCase {
     
     }
     
+    func test_load_deliversErrorsOn200ResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load {
+            capturedError.append($0)
+        }
+        let invalidJSON = Data( _ : "InvalidJson".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
     
     //MARK:  Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!)->(sut: RemoteFeedLoader,client : HTTPClientSpy ) {
@@ -130,14 +142,14 @@ class RemoteFeedLoaderTest: XCTestCase {
         func complete(with error: Error, at index: Int = 0)  {
             messages[index].completion(.failure(error))
         }
-        func complete(withStatusCode code: Int, at index: Int = 0)  {
+        func complete(withStatusCode code: Int, data : Data = Data(), at index: Int = 0)  {
             
             let response = HTTPURLResponse(url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil)!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data,response))
         }
     }
     
