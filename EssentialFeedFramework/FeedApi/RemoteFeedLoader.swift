0d6c6ec6e28fab0a556ac741868eb9c7ecd49aca
Umer <umersufyan@gmail.com>
2021-12-20 18:41:16 +0500

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeedFramework/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeedFramework/FeedApi/RemoteFeedLoader.swift
@@ -45,8 +45,8 @@ public final class RemoteFeedLoader {
             
             switch result {
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data){
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data){
+                    completion(.success(root.items))
                 }else{
                     completion(.failure(.invalidData))
                 }
@@ -59,6 +59,10 @@ public final class RemoteFeedLoader {
         }
        
     }
+}
+
+private struct Root: Decodable {
+    let items : [FeedItem]
 }
 
 
