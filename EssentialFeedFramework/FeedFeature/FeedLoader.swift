929c2b176be747a3195ce4ab9d1ea20f51debc1a
Umer <umersufyan@gmail.com>
2021-12-22 19:15:53 +0500

    Remote 'LoadFeedResult' Equatable conformance from production since it's a test-only requirement

--- EssentialFeedFramework/FeedFeature/FeedLoader.swift
+++ EssentialFeedFramework/FeedFeature/FeedLoader.swift
@@ -11,9 +11,9 @@ public enum LoadFeedResult<Error: Swift.Error> {
     case success([FeedItem])
     case failure(Error)
 }
-extension LoadFeedResult: Equatable where Error : Equatable {}
 
 protocol FeedLoader {
     associatedtype Error: Swift.Error
+    
     func load(completion: @escaping (LoadFeedResult<Error>)->Void)
 }
