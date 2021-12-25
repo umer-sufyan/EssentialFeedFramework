a68899f9f7e5bc766bfd9cb218ccc1542fe24b4a
Umer <umersufyan@gmail.com>
2021-12-25 22:15:31 +0500

    Extract test setup to a helper method

--- EssentialFeedFrameworkAPIEndToEndTests/EssentialFeedFrameworkAPIEndToEndTests.swift
+++ EssentialFeedFrameworkAPIEndToEndTests/EssentialFeedFrameworkAPIEndToEndTests.swift
@@ -11,20 +11,9 @@ import EssentialFeedFramework
 class EssentialFeedFrameworkAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_mathesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+         // ideally that should be between 3 to 5 sec
         
-        let exp = expectation(description: "Wait for load completion")
-        
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 15.0) // ideally that should be between 3 to 5 sec
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             XCTAssertEqual(items[0], expectedItem(at: 0))
@@ -50,6 +39,22 @@ class EssentialFeedFrameworkAPIEndToEndTests: XCTestCase {
     }
     
     //MARK: -Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 15.0)
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at: index), imageURL: imageURL(at: index))
