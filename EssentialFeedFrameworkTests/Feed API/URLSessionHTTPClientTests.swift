359b4e4aef8008e0acad77729eb3acbc32f171cf
Umer <umersufyan@gmail.com>
2021-12-25 10:31:48 +0500

    amend / replace anyNSError(), in place of Hardcode

--- EssentialFeedFrameworkTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedFrameworkTests/Feed API/URLSessionHTTPClientTests.swift
@@ -55,7 +55,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let requestError = NSError(domain: "any error", code: 1)
+        let requestError = anyNSError()
         
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
         
