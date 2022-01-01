8945d9310a0d1bc2092ba1d89d4c9d760217ed09
Umer <umersufyan@gmail.com>
2022-01-02 02:59:32 +0500

    Extract reusable CoreData helpers into a separate file

---
+++ EssentialFeedFramework/Feed Cache/CoreData/ManagedCache.swift
@@ -0,0 +1,27 @@
+//
+//  ManagedCache.swift
+//  EssentialFeedFramework
+//
+//  Created by Apple on 02/01/2022.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}
