a89c493b9548770424e3ad7795d374d97d449efd
Umer <umersufyan@gmail.com>
2022-01-01 23:27:42 +0500

    Improve test name to follow convention

--- EssentialFeedFrameworkTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedFrameworkTests/Feed Cache/CodableFeedStoreTests.swift
@@ -91,7 +91,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     
-    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
