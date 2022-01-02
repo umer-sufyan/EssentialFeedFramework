31319e6e98c17494f09207f45e439cd5943324aa
Umer <umersufyan@gmail.com>
2022-01-02 12:47:55 +0500

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- EssentialFeedFrameworkCacheIntegrationTests/EssentialFeedFrameworkCacheIntegrationTests.swift
+++ EssentialFeedFrameworkCacheIntegrationTests/EssentialFeedFrameworkCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import EssentialFeedFramework
 
 class EssentialFeedFrameworkCacheIntegrationTests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -38,6 +50,18 @@ class EssentialFeedFrameworkCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
