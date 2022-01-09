71aa5a23d937201dec252aa83ef4fddf1ec2d266
Umer <umersufyan@gmail.com>
2022-01-09 20:47:10 +0500

    Render loaded feed on successful load completion

---
+++ EssentialFeedFramworkiOS/FeedImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeedFramworkiOSTests
+//
+//  Created by Apple on 09/01/2022.
+//
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
