d5a3d5d4d199578f03afbbbc11e78df62253e3d6
Umer <umersufyan@gmail.com>
2022-02-11 15:41:57 +0500

    Move FeedViewModel to separate file

--- EssentialFeedFramework/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedFramework/Feed Presentation/FeedPresenter.swift
@@ -6,9 +6,6 @@
 //
 import Foundation
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
 
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
---
+++ EssentialFeedFramework/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeedFramework
+//
+//  Created by Apple on 11/02/2022.
+//
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
