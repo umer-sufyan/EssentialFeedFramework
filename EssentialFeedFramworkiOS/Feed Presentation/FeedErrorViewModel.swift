9e6089fbe4c57edc55b83c02d373d395e603044b
Umer <umersufyan@gmail.com>
2022-02-11 12:37:47 +0500

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeedFramworkiOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeedFramworkiOS
+//
+//  Created by Apple on 11/02/2022.
+//
+struct FeedErrorViewModel {
+    let message: String?
+}
--- EssentialFeedFramworkiOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedFramworkiOS/Feed Presentation/FeedPresenter.swift
@@ -7,10 +7,6 @@
 import Foundation
 import EssentialFeedFramework
 
-struct FeedErrorViewModel {
-    let message: String?
-}
-
 protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
