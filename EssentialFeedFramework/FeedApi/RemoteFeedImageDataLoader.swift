ec3076339653f9677bd5f3fbbbe617d406d22699
Umer <umersufyan@gmail.com>
2022-02-13 16:20:59 +0500

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error

--- EssentialFeedFramework/FeedApi/RemoteFeedImageDataLoader.swift
+++ EssentialFeedFramework/FeedApi/RemoteFeedImageDataLoader.swift
@@ -15,6 +15,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     }
     
     public enum Error: Swift.Error {
+        case connectivity
         case invalidData
     }
     
@@ -48,13 +49,14 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
             guard self != nil else { return }
             
             switch result {
-            case let .success(data, response):
+            case let .success((data, response)):
                 if response.statusCode == 200, !data.isEmpty {
                     task.complete(with: .success(data))
                 } else {
                     task.complete(with: .failure(Error.invalidData))
                 }
-            case let .failure(error): task.complete(with: .failure(error))
+            case .failure:
+                task.complete(with: .failure(Error.connectivity))
             }
         }
         return task
