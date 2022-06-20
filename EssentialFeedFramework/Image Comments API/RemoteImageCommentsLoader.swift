890b9dda95afc969d112c73e8e0253c8a8f591e0
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-06-20 12:31:27 +0500

    Replace RemoteImageCommentsLoader with generic RemoteLoader to remote duplication

--- EssentialFeedFramework/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeedFramework/Image Comments API/RemoteImageCommentsLoader.swift
@@ -7,46 +7,12 @@
 
 import Foundation
 
-public final class RemoteImageCommentsLoader {
-    private let url : URL
-    private let client : HTTPClient
-    
-    public enum Error: Swift.Error {  //The RemoteImageCommentLoader domain-specific Error type is a lower level implementation
-        //detail of the Feed API module. Thus we don't want it in the higher-level Feed Feature module.
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = Swift.Result<[ImageComment],Swift.Error>
-    
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    
-    public func load(completion: @escaping (Result)->Void)  {
-        
-        client.get(from: url){ [weak self] result in
-            
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteImageCommentsLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try ImageCommentsMapper.map(data, from: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentsLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: ImageCommentsMapper.map)
     }
 }
+
 
