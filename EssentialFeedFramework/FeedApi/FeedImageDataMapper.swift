4fdb5a033f2d537d0cc52d36053539c65049e7ae
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-06-23 17:56:53 +0500

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeedFramework/FeedApi/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeedFramework
+//
+//  Created by Apple on 23/06/2022.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
