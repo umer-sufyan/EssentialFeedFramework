8fd62736c6520efa4eebd092215a521de4ad3d01
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-01 22:08:19 +0500

    Rename method

--- EssentialFeedFramework/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeedFramework/Shared Presentation/LoadResourcePresenter.swift
@@ -27,7 +27,7 @@ public final class LoadResourcePresenter {
     
 
     
-    public func didStartLoadingFeed() {
+    public func didStartLoading() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
