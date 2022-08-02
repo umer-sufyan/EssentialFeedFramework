6dd2b975dba388c7024d046e8eb64a7e985a222a
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-02 17:39:43 +0500

    Remove unused FeedImagePresenter logic

--- EssentialFeedFramework/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeedFramework/Feed Presentation/FeedImageViewModel.swift
@@ -4,12 +4,10 @@
 //
 //  Created by Apple on 11/02/2022.
 //
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
+    
     
     public var hasLocation: Bool {
         return location != nil
