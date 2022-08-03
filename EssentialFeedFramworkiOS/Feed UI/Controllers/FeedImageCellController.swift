1f16a315ff9152f7910fc043e6a251178f4d67e5
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-03 09:48:17 +0500

    Add CellController protocol in preparation to support any cell controller type

--- EssentialFeedFramworkiOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeedFramworkiOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -12,7 +12,7 @@ public protocol FeedImageCellControllerDelegate {
     func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: ResourceView, ResourceLoadingView, ResourceErrorView {
+public final class FeedImageCellController:CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
     public typealias ResourceViewModel = UIImage
     
     private let viewModel: FeedImageViewModel
@@ -24,7 +24,7 @@ public final class FeedImageCellController: ResourceView, ResourceLoadingView, R
         self.delegate = delegate
     }
     
-    func view(in tableView: UITableView) -> UITableViewCell {
+    public func view(in tableView: UITableView) -> UITableViewCell {
         cell = tableView.dequeueReusableCell()
         cell?.locationContainer.isHidden = !viewModel.hasLocation
         cell?.locationLabel.text = viewModel.location
@@ -35,11 +35,11 @@ public final class FeedImageCellController: ResourceView, ResourceLoadingView, R
         return cell!
     }
     
-    func preload() {
+    public func preload() {
         delegate.didRequestImage()
     }
     
-    func cancelLoad() {
+    public func cancelLoad() {
         releaseCellForReuse()
         delegate.didCancelImageRequest()
     }
--- EssentialFeedFramworkiOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeedFramworkiOS/Feed UI/Controllers/FeedViewController.swift
@@ -12,15 +12,19 @@ public protocol FeedViewControllerDelegate {
     func didRequestFeedRefresh()
 }
 
-
+public protocol CellController {
+    func view(in tableView: UITableView) -> UITableViewCell
+    func preload()
+    func cancelLoad()
+}
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     
-    private var loadingControllers = [IndexPath: FeedImageCellController]()
+    private var loadingControllers = [IndexPath: CellController]()
    public var delegate: FeedViewControllerDelegate?
     @IBOutlet private(set) public var errorView: ErrorView?
     
-   private var tableModel = [FeedImageCellController]() {
+   private var tableModel = [CellController]() {
         didSet { tableView.reloadData() }
     }
     private var cellControllers = [IndexPath:FeedImageCellController]()
@@ -42,7 +46,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         delegate?.didRequestFeedRefresh()
     }
     
-    public func display(_ cellControllers: [FeedImageCellController]){
+    public func display(_ cellControllers: [CellController]){
         loadingControllers = [:]
         tableModel = cellControllers
     }
@@ -78,7 +82,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         indexPaths.forEach(cancelCellControllerLoad)
     }
     
-    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+    private func cellController(forRowAt indexPath: IndexPath) -> CellController {
         let controller = tableModel[indexPath.row]
         loadingControllers[indexPath] = controller
         return controller
