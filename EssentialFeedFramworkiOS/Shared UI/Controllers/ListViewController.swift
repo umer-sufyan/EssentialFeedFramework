ffc53072220cfa9a6d1f0f0c1f6f24cd8e0068bf
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-10 13:11:31 +0500

    Set data source default row animation to fade

--- EssentialFeedFramworkiOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeedFramworkiOS/Shared UI/Controllers/ListViewController.swift
@@ -26,6 +26,8 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource.defaultRowAnimation = .fade
         tableView.dataSource = dataSource
         configureErrorView()
         refresh()
