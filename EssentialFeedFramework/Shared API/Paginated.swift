d009505344f8ccc91af39d0006e1334fd2eb7fe1
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-16 13:34:13 +0500

    Make feed loader closure return Paginated<FeedImage>

---
+++ EssentialFeedFramework/Shared API/Paginated.swift
@@ -0,0 +1,19 @@
+//
+//  Paginated.swift
+//  EssentialFeedFramework
+//
+//  Created by Apple on 16/08/2022.
+//
+import Foundation
+
+public struct Paginated<Item> {
+    public typealias LoadMoreCompletion = (Result<Self, Error>) -> Void
+    
+    public let items: [Item]
+    public let loadMore: ((@escaping LoadMoreCompletion) -> Void)?
+    
+    public init(items: [Item], loadMore: ((@escaping LoadMoreCompletion) -> Void)? = nil) {
+        self.items = items
+        self.loadMore = loadMore
+    }
+}
