da6a7300916c67a295d9cdb926e4221db2065bbf
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-08-23 09:26:06 +0500

    Make FeedImageDataLoader sync

--- EssentialFeedFramework/FeedFeature/FeedImageDataLoader.swift
+++ EssentialFeedFramework/FeedFeature/FeedImageDataLoader.swift
@@ -5,13 +5,7 @@
 //  Created by Apple on 11/01/2022.
 //
 
-public protocol FeedImageDataLoaderTask {
-    func cancel()
-}
-
 public protocol FeedImageDataLoader {
     
-    typealias Result = Swift.Result<Data, Error>
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+    func loadImageData(from url: URL) throws -> Data
 }
